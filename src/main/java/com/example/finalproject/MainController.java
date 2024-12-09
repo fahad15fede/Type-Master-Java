9dd59fb350a1256fe18739d71b13a553de1b4b1f
usaidahmed01 <usaid423@gmail.com>
2024-12-09 22:58:47 +0500

    Details modes and leaderboard remaining

--- src/main/java/com/example/finalproject/MainController.java
+++ src/main/java/com/example/finalproject/MainController.java
@@ -4,34 +4,49 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 import java.util.Scanner;
 
 public class MainController {
 
-
     WordGenerator wordGenerator = new WordGenerator();
     @FXML
-    private Label givenSentence;
+    private Label accuracyPercent;
+
     @FXML
-    private Label accuracy;
+    private Label charCount;
+
+    @FXML
+    private TextFlow givenSentence;
+
+    @FXML
+    private Label timer;
 
     @FXML
     private TextField userInput;
 
     @FXML
+    private Label wordCount;
+
+    Text levels = new Text(wordGenerator.levels[0]);
+    @FXML
     public void initialize() {
-        givenSentence.setText(wordGenerator.levels[0]);
+        givenSentence.getChildren().add(levels);
+
     }
 
-    @FXML
+
         int i = 1 ;
-    public void next(ActionEvent event) {
+    @FXML
+    void onNext(ActionEvent event) {
         String getter = userInput.getText();
         Details details = new Details(getter);
 
         if(i < 7) {
-            givenSentence.setText(wordGenerator.levels[i]);
+            Text levels = new Text(wordGenerator.levels[i]);
+            givenSentence.getChildren().add(levels);
             userInput.setText(" ");
             i += 1 ;
         }
