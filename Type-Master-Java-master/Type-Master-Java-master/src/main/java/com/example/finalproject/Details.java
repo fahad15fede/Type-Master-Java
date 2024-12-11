28e915c739f27d7668796cac1f7225dc82005990
fahad15fede <fahadp0909@gmail.com>
2024-12-12 00:52:09 +0500

    Last alomst

--- Type-Master-Java-master/Type-Master-Java-master/src/main/java/com/example/finalproject/Details.java
+++ Type-Master-Java-master/Type-Master-Java-master/src/main/java/com/example/finalproject/Details.java
@@ -2,15 +2,19 @@ package com.example.finalproject;
 
 public class Details {
     static int userChoice;
-    private double Accuracy;
-    private double wordCount=0;
-    private double wpm;
-    public static double errorCountp1 = 0;
-    public static double errorCountp2 = 0;
-    public static double errorCountp3 = 0;
-    public static double characterCount1 = 0;
-    public static double characterCount2 = 0;
-    public static double characterCount3 = 0;
+    private double accuracyp1;
+    private double accuracyp2;
+    private double accuracyp3;
+    static private double wordCountp1 = 0;
+    static private double wordCountp2 = 0;
+    static private double wordCountp3 = 0;
+
+    public static int errorCountp1 = 0;
+    public static int errorCountp2 = 0;
+    public static int errorCountp3 = 0;
+    public static int characterCount1 = 0;
+    public static int characterCount2 = 0;
+    public static int characterCount3 = 0;
 
     private long timetaken;
 
@@ -32,9 +36,20 @@ public class Details {
     }
 
     public void setAccuracy() {
-        if(userChoice == 1) Accuracy = (((characterCount1-errorCountp1)/characterCount1)*100);
-        if(userChoice == 2) Accuracy = (((characterCount2-errorCountp2)/characterCount2)*100);
-        if(userChoice == 3) Accuracy = (((characterCount3-errorCountp3)/characterCount3)*100);
+        if(userChoice == 1)
+            accuracyp1 = (((double) (characterCount1 - errorCountp1) /characterCount1)*100);
+
+        if(userChoice == 2){
+            accuracyp1 = (((double) (characterCount1 - errorCountp1) /characterCount1)*100);
+            accuracyp2 = (((double) (characterCount2 - errorCountp2) / characterCount2)*100);
+        }
+
+        if(userChoice == 3){
+            accuracyp1 = (((double) (characterCount1 - errorCountp1) / characterCount1)*100);
+            accuracyp2 = (((double) (characterCount2 - errorCountp2) / characterCount2)*100);
+            accuracyp3 = (((double) (characterCount3 - errorCountp3) / characterCount3)*100);
+        }
+
     }
 
 
@@ -48,25 +63,26 @@ public class Details {
             for (int i = 0; i < user_input.length(); i++) {
 
                 if (user_input.charAt(i) != currentSentence.charAt(i)) {
-                    if (userChoice == 1) errorCountp1++;
+                    if (userChoice == 1)
+                        errorCountp1++;
                     if (userChoice == 2) {
                         if(MainController.i < 7){
                             errorCountp1++;
                         }
-                        if(MainController.i>7){
+                        if(MainController.i>=7){
                             errorCountp2++;
                         }
                     }
 
                     if (userChoice == 3) {
 
-                        if (MainController.i < 8) {
+                        if (MainController.i < 7) {
                             errorCountp1++;
                         }
-                        if (MainController.i > 8 && MainController.i < 15) {
+                        if (MainController.i > 6 && MainController.i < 14) {
                             errorCountp2++;
                         }
-                        if (MainController.i > 15 && MainController.i < 21) {
+                        if (MainController.i > 13 && MainController.i < 21) {
                             errorCountp3++;
                         }
                     }
@@ -75,25 +91,55 @@ public class Details {
 
             }
     }
-    public double getAccuracy(){
-        return this.Accuracy;
-    }
+        public double getAccuracy(){
+            if(userChoice == 1) return accuracyp1;
+            if(userChoice == 2) return accuracyp2;
+            if(userChoice == 3) return accuracyp3;
+
+            else return 0;
+        }
     public void setWpm(String user_input) {
-        wordCount = 0;
+        wordCountp1 = 0;
+        wordCountp2 = 0;
+        wordCountp3 = 0;
 
         if (!user_input.trim().isEmpty()) {
-            wordCount = 1;
+            wordCountp1 = 1;
+            wordCountp2 = 1;
+            wordCountp3 = 1;
+
             for (int i = 0; i < user_input.length(); i++) {
                 if (user_input.charAt(i) == ' ' && user_input.charAt(i - 1) != ' ') {
-                    wordCount++;
+                    if (userChoice == 1) wordCountp1++;
+                    if (userChoice == 2){
+                        if (MainController.i<7)
+                            wordCountp1++;
+                        if (MainController.i>6 && MainController.i<14)
+                            wordCountp2++;
+                    }
+                    if (userChoice == 3){
+                        if (MainController.i<7)
+                            wordCountp1++;
+                        if (MainController.i>6 && MainController.i<14)
+                            wordCountp2++;
+                        if (MainController.i>13 && MainController.i<21)
+                            wordCountp2++;
+                    }
+
                 }
             }
-            wpm=wordCount;
+
         }
     }
 
     public double getWpm() {
-        return wpm;
+        if (userChoice == 1)
+         return wordCountp1/3;
+        if (userChoice == 2)
+            return wordCountp2/3;
+        if (userChoice == 3)
+            return wordCountp3/3;
+        else return 0;
     }
 
     public double getCharacterCount(){
@@ -103,9 +149,29 @@ public class Details {
        return 0;
     }
     public void setCharacterCount(){
-        if (userChoice == 1) characterCount1 += user_input.length();
-        if (userChoice == 2) characterCount2 += user_input.length();
-        if (userChoice == 3) characterCount3 += user_input.length();
+        if (userChoice == 1)
+            characterCount1 += user_input.length();
+
+        if (userChoice == 2){
+
+                if (MainController.i < 7)
+                    characterCount1 += user_input.length();
+
+                else characterCount2 += user_input.length();
+            }
+
+        if (userChoice == 3) {
+
+                if (MainController.i < 7)
+                    characterCount1 += user_input.length();
+
+                if (MainController.i > 6 && MainController.i < 14)
+                    characterCount2 += user_input.length();
+
+                if (MainController.i > 13 && MainController.i < 21)
+                    characterCount3 += user_input.length();
+
+        }
     }
 
     public void playerSelection(int choice) {
--- Type-Master-Java-master/Type-Master-Java-master/src/main/java/com/example/finalproject/MainController.java
+++ Type-Master-Java-master/Type-Master-Java-master/src/main/java/com/example/finalproject/MainController.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 public class MainController {
 
     WordGenerator wordGenerator = new WordGenerator();
-    private int secondsLeft = 120;
+    private int secondsLeft = 60;
     private boolean timerRunning = false;
     @FXML
     private Button btn;
@@ -37,18 +37,19 @@ public class MainController {
     private TextField userInput;
 
     @FXML
-    private Label wordCount;
+    private Label turn;
 
 
     @FXML
     public void initialize() {
+        turn.setText("Player1");
         givenSentence.setText(" " + wordGenerator.levels1[0]);
-        totalChar.setText(Double.toString(Details.characterCount1));
-        totalErrors.setText(Double.toString(Details.errorCountp1));
+        totalChar.setText(Integer.toString(Details.characterCount1));
+        totalErrors.setText(Integer.toString(Details.errorCountp1));
     }
 
 
-       public static int i = 1 ;
+       public static int i = 0 ;
         public Details details;
     @FXML
     void onNext(ActionEvent event)  {
@@ -56,15 +57,21 @@ public class MainController {
 
 
         if(Details.userChoice == 1) {
-            wordCount.setText("Player1");
-            totalChar.setText(Double.toString(Details.characterCount1));
-            totalErrors.setText(Double.toString(Details.errorCountp1));
+
+            totalChar.setText(Integer.toString(Details.characterCount1));
+            totalErrors.setText(Integer.toString(Details.errorCountp1));
             if (i < 7) {
                 String user_type = userInput.getText();
-                details = new Details( wordGenerator.levels1[i - 1] ,user_type );
-                totalChar.setText(Double.toString(Details.characterCount1));
-                totalErrors.setText(Double.toString(Details.errorCountp1));
-                givenSentence.setText(" " + wordGenerator.levels1[i]);
+                details = new Details( wordGenerator.levels1[i] ,user_type );
+                totalChar.setText(Integer.toString(Details.characterCount1));
+                totalErrors.setText(Integer.toString(Details.errorCountp1));
+                if( i == 6 ){
+                    givenSentence.setText(" " + wordGenerator.levels1[i]);
+                    btn.setVisible(false);
+                }else{
+                    givenSentence.setText(" " + wordGenerator.levels1[i+1]);
+
+                }
 
                 userInput.setText("");
                 i += 1;
@@ -73,51 +80,71 @@ public class MainController {
 
         if(Details.userChoice == 2) {
             if (i < 14) {
-                if(i <= 7){
-                    wordCount.setText("Player1");
-                    totalChar.setText(Double.toString(Details.characterCount1));
-                    totalErrors.setText(Double.toString(Details.errorCountp1));
-                }
-                if ( i > 7 && i <= 14){
-                    wordCount.setText("Player2");
-
-                    totalChar.setText(Double.toString(Details.characterCount2));
-                    totalErrors.setText(Double.toString(Details.errorCountp2));
-                }
 
                 String user_type = userInput.getText();
-                details = new Details( wordGenerator.levels2[i - 1] ,user_type );
-                givenSentence.setText(" " + wordGenerator.levels2[i]);
+                details = new Details( wordGenerator.levels2[i] ,user_type );
+                if( i == 13 ){
+                    givenSentence.setText(" " + wordGenerator.levels2[i]);
+                    btn.setVisible(false);
+                }else{
+                    givenSentence.setText(" " + wordGenerator.levels2[i+1]);
+                }
+                if (i == 6) stopTimer();
+                if(i < 7){
+                    turn.setText("Player 1");
+                    totalChar.setText(Integer.toString(Details.characterCount1));
+                    totalErrors.setText(Integer.toString(Details.errorCountp1));
+                }
 
+                if ( i >= 6 && i < 14){
+
+                    turn.setText("Player 2");
+                    totalChar.setText(Integer.toString(Details.characterCount2));
+                    totalErrors.setText(Integer.toString(Details.errorCountp2));
+                }
                 userInput.setText("");
-                i += 1;
+                i+=1;
             }
 
 
         }
         if(Details.userChoice == 3) {
-            if(i <= 7){
-
-                totalChar.setText(Double.toString(Details.characterCount1));
-                totalErrors.setText(Double.toString(Details.errorCountp1));
-            }
-            if ( i > 7 && i <= 14){
-                totalChar.setText(Double.toString(Details.characterCount2));
-                totalErrors.setText(Double.toString(Details.errorCountp2));
-            }
-            if (i > 14 && i <= 21){
-                totalChar.setText(Double.toString(Details.characterCount3));
-                totalErrors.setText(Double.toString(Details.errorCountp3));
-            }
             if (i < 21) {
-                String user_type = userInput.getText();
-                details = new Details( wordGenerator.levels3[i - 1] ,user_type );
-                totalChar.setText(Double.toString(Details.characterCount3));
-                totalErrors.setText(Double.toString(Details.errorCountp3));
-                givenSentence.setText(" " + wordGenerator.levels3[i]);
 
-                userInput.setText("");
-                i += 1;
+                String user_type = userInput.getText();
+                details = new Details( wordGenerator.levels3[i] ,user_type );
+                totalChar.setText(Integer.toString(Details.characterCount3));
+                totalErrors.setText(Integer.toString(Details.errorCountp3));
+
+                if( i == 20 ){
+                    givenSentence.setText(" " + wordGenerator.levels3[i]);
+                    btn.setVisible(false);
+                }
+                else {
+                    givenSentence.setText(" " + wordGenerator.levels3[i + 1]);
+                }
+                if (i == 6) stopTimer();
+                if (i == 13) stopTimer();
+            if(i < 7){
+
+                turn.setText("Player 1");
+                totalChar.setText(Integer.toString(Details.characterCount1));
+                totalErrors.setText(Integer.toString(Details.errorCountp1));
+            }
+            if ( i >= 6 && i < 14){
+
+                turn.setText("Player 2");
+                totalChar.setText(Integer.toString(Details.characterCount2));
+                totalErrors.setText(Integer.toString(Details.errorCountp2));
+            }
+            if (i >= 13 && i < 21){
+
+                turn.setText("Player 3");
+                totalChar.setText(Integer.toString(Details.characterCount3));
+                totalErrors.setText(Integer.toString(Details.errorCountp3));
+            }
+            userInput.setText("");
+            i += 1;
             }
         }
     }
@@ -127,12 +154,18 @@ public class MainController {
         if (!timerRunning) {
             startTimer();
         }
+//        if (Details.userChoice == 2){
+//            if(i == 7){
+//                startTimer();
+//            }
+//        }
     }
 
+    private Timeline timeline;
     @FXML
     private void startTimer() {
         timerRunning = true;
-        Timeline timeline = new Timeline(
+        timeline = new Timeline(
                 new KeyFrame(Duration.seconds(1), e -> {
                     try {
                         updateTimer();
@@ -155,5 +188,11 @@ public class MainController {
             btn.setVisible(false);
         }
     }
-
+    private void stopTimer() {
+        if (timeline != null) {
+            timeline.stop();
+            timerRunning = false;
+            secondsLeft = 60;
+        }
+    }
 }
